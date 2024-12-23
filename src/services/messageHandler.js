1f9226e517752e48c3e780da538346a94c7c8f0b
wicaraguay <wicaraguay@gmail.com>
2024-12-23 13:47:26 -0500

    Add code image, video, document and audio

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -7,10 +7,17 @@ class MessageHandler {
 
       if(this.isGreeting(incomingMessage)){
         await this.sendWelcomeMessage(message.from, message.id, senderInfo);
-      } else {
+        await this.sendWelcomeMenu(message.from);
+      }else if(incomingMessage === 'media') {
+        await this.sendMedia(message.from);
+      }else {
         const response = `Echo: ${message.text.body}`;
         await whatsappService.sendMessage(message.from, response, message.id);
       }
+      await whatsappService.markAsRead(message.id);
+    }else if (message?.type === 'interactive') {
+      const option = message?.interactive?.button_reply?.title.toLowerCase().trim();
+      await this.handleMenuOption(message.from, option);
       await whatsappService.markAsRead(message.id);
     }
   }
@@ -29,6 +36,57 @@ class MessageHandler {
     const welcomeMessage = `Hola ${name.split(' ')[0]}, Bienvenido a ECO SHOES, Tu tienda de calzado en línea. ¿En qué puedo ayudarte hoy?`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
   }
+  async sendWelcomeMenu (to){
+    const menuMessage = "Elige una opción:";
+    const buttons = [
+      {
+        type: 'reply', reply: {id: 'option_1', title: 'Ver catalogo'}
+      },
+      {
+        type: 'reply', reply: {id: 'option_2', title: 'Consultar'}
+      },
+      {
+        type: 'reply', reply: {id: 'option_3', title: 'Ubicacion'}
+      }
+    ]
+    await whatsappService.sendInterativeButtons(to, menuMessage, buttons);
+  }
+  async handleMenuOption(to, option){
+    let response;
+    switch (option){
+      case 'ver catalogo':
+        response = "Ver catálogo";
+        break;
+      case 'consultar': 
+        response = "Realiza tu Consultar";
+        break;
+      case 'ubicacion':
+        response = "Esta es nuestra ubicación";
+        break;
+      default:
+        response = "Lo siento no entendi tu seleccion. Por favor, elije una de la opciones del menu";
+    }
+    await whatsappService.sendMessage(to, response);  
+  }
+  async sendMedia(to){
+    // const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-audio.aac';
+    // const caption = 'Bienvenida';
+    // const type = 'audio';
+
+    // const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-imagen.png';
+    // const caption = '¡Esto es una Imagen!';
+    // const type = 'image';
+
+    const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-video.mp4';
+    const caption = '¡Esto es una video!';
+    const type = 'video';
+
+    // const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-file.pdf';
+    // const caption = '¡Esto es un PDF!';
+    // const type = 'document';
+    await whatsappService.sendMediaMessage(to, type, mediaUrl, caption);
+  }
+
 
 }
 
--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -14,9 +14,9 @@ class WhatsAppService {
           messaging_product: 'whatsapp',
           to,
           text: { body },
-          context: {
-            message_id: messageId,
-          },
+          // context: {
+          //   message_id: messageId,
+          // },
         },
       });
     } catch (error) {
@@ -40,6 +40,70 @@ class WhatsAppService {
       });
     } catch (error) {
       console.error('Error marking message as read:', error);
+    }
+  }
+
+  async sendInterativeButtons(to, BodyText, buttons) {
+    try{
+      await axios({
+        method: 'POST',
+        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        headers: {
+          Authorization: `Bearer ${config.API_TOKEN}`,
+        },
+        data: {
+          messaging_product: 'whatsapp',
+          to,
+          type: 'interactive',
+          interactive: {
+            type: 'button',
+            body: {text: BodyText},
+            action:{buttons: buttons}
+          },
+        },
+      });
+    }catch (error) {
+      console.error(error);
+    }
+  }
+
+  async sendMediaMessage (to, type, media_URL, caption){
+    try{
+      const mediaObject = {};
+
+      switch (type){
+        case 'image':
+          mediaObject.image = {link: media_URL, caption: caption};
+          break;
+        case 'audio':
+          mediaObject.audio = {link: media_URL};
+          break;
+        case 'video':
+          mediaObject.video = {link: media_URL, caption: caption};
+          break;
+        case 'document':
+          mediaObject.document = {link: media_URL, caption: caption, filename:'medpet-file.pdf'};
+          break;
+          default:
+            throw new Error('Not soported media type');
+      }
+      await axios({
+        method: 'POST',
+        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        headers: {
+          Authorization: `Bearer ${config.API_TOKEN}`,
+        },
+        data: {
+          messaging_product: 'whatsapp',
+          to,
+          type: type,
+          ...mediaObject
+        },
+      })
+
+
+    }catch (error){
+      console.error('Error sending media',error);
     }
   }
 }
